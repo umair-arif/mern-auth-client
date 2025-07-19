501e865cd483884a823e0a6a2a9b0c8e8eeed249
umair-arif <umair.aarifs@gmail.com>
2025-07-19 13:48:34 +0500

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[url('/bg_img.png')] bg-cover bg-center">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[url('../assets/bg_img.png')] bg-cover bg-center">
       <Navbar />
       <Header />
     </div>
